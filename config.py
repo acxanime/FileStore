76b9ed0d89f38207da186fa609850f768b0d76d2
acxanime <acxanime07@gmail.com>
2025-09-06 11:50:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -91,13 +91,13 @@ USER_REPLY_TEXT = "ʙᴀᴋᴋᴀ ! ʏᴏᴜ ᴀʀᴇ ɴᴏᴛ ᴍʏ ꜱᴇɴᴘ
 #==========================(BUY PREMIUM)====================#
 
 OWNER_TAG = os.environ.get("OWNER_TAG", "rohit_1888")
-UPI_ID = os.environ.get("UPI_ID", "rohit23pnb@axl")
-QR_PIC = os.environ.get("QR_PIC", "https://telegra.ph/file/3e83c69804826b3cba066-16cffa90cd682570da.jpg")
-SCREENSHOT_URL = os.environ.get("SCREENSHOT_URL", f"t.me/rohit_1888")
+UPI_ID = os.environ.get("UPI_ID", "acxanime@upi")
+QR_PIC = os.environ.get("QR_PIC", "https://graph.org/file/d71d50f68086bd63b2277-6a2c66ff2a02ff5982.jpg")
+SCREENSHOT_URL = os.environ.get("SCREENSHOT_URL", f"t.me/sitaratoons_support")
 #--------------------------------------------
 #Time and its price
 #7 Days
-PRICE1 = os.environ.get("PRICE1", "0 rs")
+PRICE1 = os.environ.get("PRICE1", "30 rs")
 #1 Month
 PRICE2 = os.environ.get("PRICE2", "60 rs")
 #3 Month
